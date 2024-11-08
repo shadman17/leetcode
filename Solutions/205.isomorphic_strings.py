1425b5676da3888981544a0342c6d91a30ae82a7
Shadman Wasif <shadman.wasif@gmail.com>
2024-11-09 01:17:38 +0600

    Happy Day, solved lots of problems

--- Solutions/205.isomorphic_strings.py
+++ Solutions/205.isomorphic_strings.py
@@ -1,26 +1,29 @@
 from collections import Counter
 
+
 class Solution1:
     def isIsomorphic(self, s: str, t: str) -> bool:
         return len(set(zip(s, t))) == len(set(s)) == len(set(t))
 
-t = Solution1()
-print(t.isIsomorphic(s = "bbbaaaba", t = "aaabbbba"))
-print(t.isIsomorphic(s = "egg", t = "add"))
-print(t.isIsomorphic(s = "paper", t = "title"))
-print(t.isIsomorphic(s = "foo", t = "bar"))
-print(t.isIsomorphic(s = "badc", t = "baba"))
+
+# t = Solution1()
+# print(t.isIsomorphic(s = "bbbaaaba", t = "aaabbbba"))
+# print(t.isIsomorphic(s = "egg", t = "add"))
+# print(t.isIsomorphic(s = "paper", t = "title"))
+# print(t.isIsomorphic(s = "foo", t = "bar"))
+# print(t.isIsomorphic(s = "badc", t = "baba"))
+
 
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         new_dict = {}
         new_dict2 = {}
-         
+
         for i, char in enumerate(s):
             # print(i, char)
             if char not in new_dict:
                 new_dict[char] = t[i]
-   
+
             else:
                 if new_dict[char] != t[i]:
                     return False
@@ -29,7 +32,7 @@ class Solution:
             # print(i, char)
             if char not in new_dict2:
                 new_dict2[char] = s[i]
-   
+
             else:
                 if new_dict2[char] != s[i]:
                     return False
@@ -38,9 +41,8 @@ class Solution:
 
 
 s = Solution()
-print(s.isIsomorphic(s = "bbbaaaba", t = "aaabbbba"))
-print(s.isIsomorphic(s = "egg", t = "add"))
-print(s.isIsomorphic(s = "paper", t = "title"))
-print(s.isIsomorphic(s = "foo", t = "bar"))
-print(s.isIsomorphic(s = "badc", t = "baba"))
-
+print(s.isIsomorphic(s="bbbaaaba", t="aaabbbba"))
+print(s.isIsomorphic(s="egg", t="add"))
+print(s.isIsomorphic(s="paper", t="title"))
+print(s.isIsomorphic(s="foo", t="bar"))
+print(s.isIsomorphic(s="badc", t="baba"))
