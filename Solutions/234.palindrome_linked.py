32869f870fb44aa22bea208abeef4f3d8a436124
shadman17 <shadman.wasif@gmail.com>
2024-11-26 16:01:41 +0600

    Updated

--- Solutions/234.palindrome_linked.py
+++ Solutions/234.palindrome_linked.py
@@ -15,3 +15,29 @@ class Solution:
             head = head.next
         
         return result == result[::-1]
+    
+
+# class Solution:
+#     def reverse(self, head: ListNode) -> ListNode:
+#         prev = None
+#         curr = head
+#         while curr:
+#             next_temp = curr.next
+#             curr.next = prev
+#             prev = curr
+#             curr = next_temp
+#         return prev
+
+#     def isPalindrome(self, head: Optional[ListNode]) -> bool:
+#         slow = head
+#         fast = head
+#         while fast and fast.next:
+#             slow = slow.next
+#             fast = fast.next.next
+#         rev = self.reverse(slow)
+#         while rev:
+#             if head.val != rev.val:
+#                 return False
+#             head = head.next
+#             rev = rev.next
+#         return True
