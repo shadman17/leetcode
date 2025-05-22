14205d865f69010849e8733c32983264b06ab47a
shadman17 <shadman.wasif@gmail.com>
2025-05-22 13:56:18 +0600

    updated

--- Solutions/17.letter_combinations.py
+++ Solutions/17.letter_combinations.py
@@ -1,12 +1,15 @@
+from typing import List
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         
         res = []
 
         digitmap = {"2":"abc", "3":"def", "4":"ghi", "5":"jkl", "6":"mno", "7":"pqrs", "8":"tuv", "9":"wxyz"}
-
+        count = 0
         def backtrack(i, curstr):
             if len(curstr) == len(digits):
+                nonlocal count 
+                count = count + 1
                 res.append(curstr)
                 return
             
@@ -15,4 +18,9 @@ class Solution:
 
         if digits:
             backtrack(0, "")
+
+        print(count)
         return res
+    
+s = Solution()
+print(s.letterCombinations("23"))
