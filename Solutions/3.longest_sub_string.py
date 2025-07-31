9d6c992fd70c0c2a6abbf06603ee0d9db0f9682a
Shadman Wasif <shadman.wasif@gmail.com>
2025-07-31 11:21:07 +0600

    updated with patterns

--- Solutions/3.longest_sub_string.py
+++ Solutions/3.longest_sub_string.py
@@ -13,6 +13,27 @@ class Solution:
                 left += 1
 
             char_set.add(s[right])
-            max_len = max(max_len, right-left+1)
+            max_len = max(max_len, right - left + 1)
+
+        return max_len
+
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if len(s) == 0:
+            return 0
+
+        l, r = 0, 0
+        hashmap = defaultdict(int)
+        max_len = 0
+
+        while r < len(s):
+            if hashmap[s[r]] != -1:
+                if hashmap[s[r]] >= l:
+                    l = hashmap[s[r]] + 1
+
+            max_len = max(max_len, r - l + 1)
+            hashmap[s[r]] = r
+            r += 1
 
         return max_len
