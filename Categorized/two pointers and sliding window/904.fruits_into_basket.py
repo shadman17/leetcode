b8871a868c241ceb8b22022c5383ebed30d93e97
Shadman Wasif <shadman.wasif@gmail.com>
2025-08-02 20:13:30 +0600

    updated

--- Categorized/two pointers and sliding window/904.fruits_into_basket.py
+++ Categorized/two pointers and sliding window/904.fruits_into_basket.py
@@ -1,6 +1,25 @@
 from collections import defaultdict
 from typing import List
 
+# class Solution:
+#     def totalFruit(self, fruits: List[int]) -> int:
+#         l, r, maxlen = 0, 0, 0
+#         hashmap = defaultdict(int)
+
+#         while r < len(fruits):
+#             hashmap[fruits[r]] += 1
+
+#             while len(hashmap) > 2:
+
+#                 hashmap[fruits[l]] -= 1
+#                 if hashmap[fruits[l]] == 0:
+#                     del hashmap[fruits[l]]
+#                 l += 1
+
+#             maxlen = max(maxlen, r - l + 1)
+#             r += 1
+#         return maxlen
+
 
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
@@ -10,8 +29,7 @@ class Solution:
         while r < len(fruits):
             hashmap[fruits[r]] += 1
 
-            while len(hashmap) > 2:
-                print(hashmap)
+            if len(hashmap) > 2:
 
                 hashmap[fruits[l]] -= 1
                 if hashmap[fruits[l]] == 0:
