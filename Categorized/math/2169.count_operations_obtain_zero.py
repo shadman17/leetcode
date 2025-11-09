4ee7dd138bb02b5f09f66f6d2011a186b72e3e63
Shadman Wasif <shadman.wasif@gmail.com>
2025-11-09 21:13:51 +0600

    updated

---
+++ Categorized/math/2169.count_operations_obtain_zero.py
@@ -0,0 +1,25 @@
+# class Solution:
+#     def countOperations(self, num1: int, num2: int) -> int:
+#         op = 0
+#         while num1 > 0 and num2 > 0:
+#             if num1 >= num2:
+#                 num1 -= num2
+#             else:
+#                 num2 -= num1
+
+#             op += 1
+        
+#         return op
+    
+class Solution:
+    def countOperations(self, num1: int, num2: int) -> int:
+        op = 0
+        while num1 > 0 and num2 > 0:
+            if num1 >= num2:
+                op += num1 // num2
+                num1 = num1 % num2
+            else:
+                op += num2 // num1
+                num2 = num2 % num1
+
+        return op
