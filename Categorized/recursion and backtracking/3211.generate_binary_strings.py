a309a0fe6d8c652daa095d53192d560ee9efa2ad
Shadman Wasif <shadman.wasif@gmail.com>
2025-11-15 19:10:29 +0600

    updated

---
+++ Categorized/recursion and backtracking/3211.generate_binary_strings.py
@@ -0,0 +1,42 @@
+class Solution:
+    def validStrings(self, n: int) -> List[str]:
+        res = []
+
+        def backtrack(curr):
+            # If we've built a string of length n, record it
+            if len(curr) == n:
+                res.append(curr)
+                return
+
+            # Option 1: put '1' (always allowed)
+            backtrack(curr + '1')
+
+            # Option 2: put '0' (only if previous isn't '0')
+            if not curr or curr[-1] != '0':
+                backtrack(curr + '0')
+
+        backtrack("")
+        return res
+
+
+class Solution:
+    def validStrings(self, n: int) -> List[str]:
+        res = []
+        
+        def backtrack(ind, zerocount, arr):
+            if len(arr) == n:
+                res.append("".join(arr))
+                return
+            if zerocount < 1:
+                arr.append("0")
+                backtrack(ind + 1, zerocount+1, arr)
+                arr.pop()
+            
+            zerocount = 0
+            
+            arr.append("1")
+            backtrack(ind + 1, zerocount, arr)
+            arr.pop()
+
+        backtrack(0, 0, [])
+        return res
