c19d1d868b10e02726666e9bdd82edbe10d44788
Shadman Wasif <shadman.wasif@gmail.com>
2025-12-19 02:58:33 +0600

    updated

--- Categorized/monotonic stack/503.next_greater_2.py
+++ Categorized/monotonic stack/503.next_greater_2.py
@@ -1,27 +1,47 @@
 from typing import List
 
+
 class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
-        
-        result = [-1] * len(nums)
+        n = len(nums)
         stack = []
-        length = len(nums)
+        res = [-1] * n
 
-        for i in range(2 * length):
-            cur = nums[i%length]
+        for i in range(2 * n - 1, -1, -1):
+            val = nums[i % n]
+            while stack and stack[-1] <= val:
+                stack.pop()
 
-            while stack and cur > nums[stack[-1]]:
-                idx = stack.pop()
-                result[idx] = cur
-            
-            if i < length:
-                stack.append(i)
+            if i < n:
+                res[i] = -1 if not stack else stack[-1]
 
-        return result
-    
+            stack.append(val)
+
+        return res
+
+
 # class Solution:
 #     def nextGreaterElements(self, nums: List[int]) -> List[int]:
-        
+
+#         result = [-1] * len(nums)
+#         stack = []
+#         length = len(nums)
+
+#         for i in range(2 * length):
+#             cur = nums[i%length]
+
+#             while stack and cur > nums[stack[-1]]:
+#                 idx = stack.pop()
+#                 result[idx] = cur
+
+#             if i < length:
+#                 stack.append(i)
+
+#         return result
+
+# class Solution:
+#     def nextGreaterElements(self, nums: List[int]) -> List[int]:
+
 #         stack, res = [], [-1] * len(nums)
 #         for i, num in enumerate(nums):              # 2
 #             while stack and nums[stack[-1]] < num:
@@ -33,6 +53,6 @@ class Solution:
 #         return res
 
 s = Solution()
-print(s.nextGreaterElements(nums = [1,2,1]))
-print(s.nextGreaterElements(nums = [1,2,3,4,3]))
+print(s.nextGreaterElements(nums=[1, 2, 1]))
+print(s.nextGreaterElements(nums=[1, 2, 3, 4, 3]))
 # print(s.nextGreaterElements())
