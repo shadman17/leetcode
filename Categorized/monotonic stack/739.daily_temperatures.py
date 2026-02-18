f5e1711275c5797b9b3de041f730b190d71aff8d
Shadman Wasif <shadman.wasif@gmail.com>
2026-02-18 16:09:16 +0600

    updated

---
+++ Categorized/monotonic stack/739.daily_temperatures.py
@@ -0,0 +1,14 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        n = len(temperatures)
+        stack = []
+        nge = [0] * n
+        for i in range(n - 1, -1, -1):
+
+            while stack and temperatures[stack[-1]] <= temperatures[i]:
+                stack.pop()
+
+            nge[i] = 0 if not stack else (stack[-1] - i)
+
+            stack.append(i)
+        return nge
